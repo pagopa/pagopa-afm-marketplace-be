e006c1fc2fb93a6fbe2c3caa46df62479ffbe88f
MarcoAGreco <marcoandreagreco@gmail.com>
2022-06-08 10:43:42 +0200

    [PPR-355] feat: spring boot app setup

---
+++ src/main/java/it/pagopa/afm/marketplacebe/MarketplaceBeApplication.java
@@ -0,0 +1,13 @@
+package it.pagopa.afm.marketplacebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/pagopa/afm/marketplacebe/MarketplaceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pagopa.afm.marketplacebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
