ac45b00c5215001d4c7c1114266b065cca1e3212
Jacopo <jacopo.carlini@gmail.com>
2022-06-17 09:57:16 +0200

    fix test

--- src/test/java/it/pagopa/afm/marketplacebe/MarketplaceBeApplicationTests.java
+++ src/test/java/it/pagopa/afm/marketplacebe/MarketplaceBeApplicationTests.java
@@ -1,15 +1,12 @@
 package it.pagopa.afm.marketplacebe;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
 class MarketplaceBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Test
+    void contextLoads() {
+    }
 
 }
