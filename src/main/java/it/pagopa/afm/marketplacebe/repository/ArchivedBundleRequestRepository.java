4ff953cfedc52397914955dc997605405762fd8d
Jacopo <jacopo.carlini@gmail.com>
2022-07-11 12:45:29 +0200

    fix repository

--- src/main/java/it/pagopa/afm/marketplacebe/repository/ArchivedBundleRequestRepository.java
+++ src/main/java/it/pagopa/afm/marketplacebe/repository/ArchivedBundleRequestRepository.java
@@ -2,28 +2,9 @@ package it.pagopa.afm.marketplacebe.repository;
 
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
 import it.pagopa.afm.marketplacebe.entity.ArchivedBundleRequest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ArchivedBundleRequestRepository extends CosmosRepository<ArchivedBundleRequest, String> {
-
-    Page<ArchivedBundleRequest> findByIdPsp(String idPsp, Pageable pageable);
-
-    Page<ArchivedBundleRequest> findByIdPspAndCiFiscalCode(String idPsp, String fiscalCode, Pageable pageable);
-
-    List<ArchivedBundleRequest> findByIdPsp(String idPsp);
-
-    List<ArchivedBundleRequest> findByIdBundleAndIdPspAndAcceptedDateIsNullAndRejectionDateIsNull(String idBundle, String idPsp);
-
-    Optional<ArchivedBundleRequest> findByIdAndIdPsp(String id, String idPsp);
-
-    List<ArchivedBundleRequest> findByCiFiscalCode(String fiscalCode);
-
-    List<ArchivedBundleRequest> findByCiFiscalCodeAndIdPsp(String fiscalCode, String idPsp);
 
 }
