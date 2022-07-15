8cd4aa9f843cfad8bf277ac10454f7a7a13657f8
MarcoAGreco <marcoandreagreco@gmail.com>
2022-07-15 11:09:15 +0200

    [PGEC-40] fix: ArchivedBundleOfferRepository annotations

--- src/main/java/it/pagopa/afm/marketplacebe/repository/ArchivedBundleOfferRepository.java
+++ src/main/java/it/pagopa/afm/marketplacebe/repository/ArchivedBundleOfferRepository.java
@@ -2,6 +2,9 @@ package it.pagopa.afm.marketplacebe.repository;
 
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
 import it.pagopa.afm.marketplacebe.entity.ArchivedBundleOffer;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArchivedBundleOfferRepository extends CosmosRepository<ArchivedBundleOffer, String> {
+
 }
