86a643a1cd13835484268b99f5da62984ae79bed
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-04-24 10:18:39 +0200

    PAGOPA-1666 adding openapi technical support creation

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -5,3 +5,13 @@ if [[ "$(pwd)" =~ .*"openapi".* ]]; then
 fi
 
 mvn test -Dtest=OpenApiGenerationTest
+
+jq 'del( .paths[].post, .paths[].put, .paths[].delete)' ./openapi/openapi.json > ./openapi/openapi-technical-support.json
+
+removeList=$(jq '.paths | to_entries[] | select(.value.get == null) | .key' ./openapi/openapi-technical-support.json)
+
+for element in $removeList
+do
+  cmd="jq 'del(.paths[${element}])' ./openapi/openapi-technical-support.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-technical-support.json"
+  eval $cmd
+done
