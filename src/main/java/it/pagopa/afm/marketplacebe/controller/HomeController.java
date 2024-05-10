befdae537cc5a3d4fa9b9359fa495a41362116be
gioelemella <128155546+gioelemella@users.noreply.github.com>
2024-05-10 15:00:03 +0200

    [VAS-1025] fix: ValidityDateFrom filter (#104)
    * [VAS-1025] change filter type
    
    * Bump to version 0.15.7-1-VAS-1025-fix-validity-date-from-filter [skip ci]
    
    * [VAS-1025] fix unit tests
    
    * Bump to version 0.15.7-2-VAS-1025-fix-validity-date-from-filter [skip ci]
    
    * [VAS-1025] updated openapi
    
    * [VAS-1025] fix date format and updated unit tests and openapi
    
    * [VAS-1025] remove imports
    
    * [VAS-1025] example
    
    ---------
    
    Co-authored-by: giomella <gioele.mella@emeal.nttdata.com>
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>
    Co-authored-by: Jacopo <jacopo.carlini@gmail.com>

--- src/main/java/it/pagopa/afm/marketplacebe/controller/HomeController.java
+++ src/main/java/it/pagopa/afm/marketplacebe/controller/HomeController.java
@@ -15,6 +15,7 @@ import it.pagopa.afm.marketplacebe.model.bundle.Bundles;
 import it.pagopa.afm.marketplacebe.service.BundleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -114,7 +115,7 @@ public class HomeController {
     public Bundles getGlobalBundles(
             @Parameter(description = "Bundle's type") @RequestParam(required = false, defaultValue = "GLOBAL") @Valid List<BundleType> types,
             @Parameter(description = "Bundle's name") @RequestParam(required = false) @Valid String name,
-            @Parameter(description = "Validity date of bundles, used to retrieve all bundles valid from the specified date") @RequestParam(required = false) LocalDate validFrom,
+            @Parameter(description = "Validity date of bundles, used to retrieve all bundles valid from the specified date (yyyy-MM-dd)", example = "2024-05-10") @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate validFrom,
             @Parameter(description = "Number of items for page") @RequestParam(required = false, defaultValue = "50") @Positive Integer limit,
             @Parameter(description = "Page number") @RequestParam(required = false, defaultValue = "0") @Min(0) @PositiveOrZero Integer page
     ) {
