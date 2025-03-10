8cfdc5bc8136528937112982e56369765ef6749a
svariant <samuele.varianti@nttdata.com>
2025-03-10 14:32:58 +0100

    feat: [PAGOPA-2659] Add other containers to onus patch script

--- utilities/patchOnUsBundles/script.js
+++ utilities/patchOnUsBundles/script.js
@@ -2,37 +2,42 @@ import { CosmosClient } from "@azure/cosmos";
 
 const key = "<cosmos-key>";
 const endpoint = "<cosmos-endpoint>";
-const containerId = "bundles";
 const databaseId = "db";
 
 const client = new CosmosClient({ endpoint, key });
 const { database } = await client.databases.createIfNotExists({ id: databaseId });
-const { container } = await database.containers.createIfNotExists({ id: containerId });
 
-let numberOfPatch = 0;
+const CONTAINER_ID_BUNDLES = "bundles";
+const CONTAINER_ID_VALID_BUNDLES = "validbundles";
+const CONTAINER_ID_ARCHIVED_BUNDLES = "archivedbundles";
 
-async function patchObject(bundleId, partitionKey, onUsValue) {
+async function patchObject(container, bundleId, partitionKey, onUsValue) {
     const operations =
         [
             { op: 'add', path: '/onus', value: onUsValue },
         ];
 
     await container.item(bundleId, partitionKey).patch(operations);
-
-    numberOfPatch += 1;
 }
 
-async function run() {
+async function run(containerId) {
+    let numberOfPatch = 0;
+
+    const { container } = await database.containers.createIfNotExists({ id: containerId });
 
     const { resources } = await container.items.readAll().fetchAll();
 
-    console.log("Number of Bundles: ", resources.length);
+    console.log(`Number of ${containerId}: `, resources.length);
 
     for (const bundle of resources) {
-        await patchObject(bundle.id, bundle.idPsp, bundle.name.includes("_ONUS"));
+        await patchObject(container, bundle.id, bundle.idPsp, bundle.name.includes("_ONUS"));
+        numberOfPatch += 1;
     }
 
-    console.log("Number of patches: ", numberOfPatch);
+    console.log(`Number of patches for ${containerId}: `, numberOfPatch);
+    console.log("\n");
 }
 
-run().catch((err) => console.error("Error: ", err));
+await run(CONTAINER_ID_BUNDLES).catch((err) => console.error(`Error container ${CONTAINER_ID_BUNDLES}: `, err));
+await run(CONTAINER_ID_VALID_BUNDLES).catch((err) => console.error(`Error container ${CONTAINER_ID_VALID_BUNDLES}: `, err));
+await run(CONTAINER_ID_ARCHIVED_BUNDLES).catch((err) => console.error(`Error container ${CONTAINER_ID_ARCHIVED_BUNDLES}: `, err));
