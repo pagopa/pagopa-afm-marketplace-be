b0ae91fc1174512113329eef14d8c2d43bd57693
Jacopo Carlini <jacopo.carlini@gmail.com>
2025-07-15 15:40:01 +0200

    feat: add crud payment methods (#136)
    * feat: add crud payment methods
    
    * Bump to version 0.21.14-1-crud-payment-methods [skip ci]
    
    * Bump to version 0.21.14-2-crud-payment-methods [skip ci]
    
    * fix
    
    * Bump to version 0.21.14-3-crud-payment-methods [skip ci]
    
    * fix
    
    * fix
    
    * Bump to version 0.21.14-4-crud-payment-methods [skip ci]
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

---
+++ src/main/java/it/pagopa/afm/marketplacebe/model/paymentmethods/enums/PaymentMethodGroup.java
@@ -0,0 +1,13 @@
+package it.pagopa.afm.marketplacebe.model.paymentmethods.enums;
+
+public enum PaymentMethodGroup {
+    CP,
+    MYBK,
+    BPAY,
+    PPAL,
+    RPIC,
+    RBPS,
+    SATY,
+    APPL,
+    RICO,
+}
