14c3a8633ad31fc2821bfe2404669915530059af
Francesco Cesareo <cesareo.francesco@gmail.com>
2025-10-03 10:24:11 +0200

    fix identity (#139)

--- .identity/03_github_environment.tf
+++ .identity/03_github_environment.tf
@@ -40,6 +40,8 @@ locals {
     "BOT_TOKEN_GITHUB" : data.azurerm_key_vault_secret.key_vault_bot_cd_token.value,
     "CUCUMBER_PUBLISH_TOKEN" : data.azurerm_key_vault_secret.key_vault_cucumber_token.value,
     "SUBKEY" : data.azurerm_key_vault_secret.key_vault_integration_test_subkey.value,
+    "SLACK_WEBHOOK_URL_INTEGRATION_TEST": data.azurerm_key_vault_secret.key_vault_integration_test_webhook_slack.value,
+    "SLACK_WEBHOOK_URL_DEPLOY": data.azurerm_key_vault_secret.key_vault_deploy_webhook_slack.value
   }
 }
 
